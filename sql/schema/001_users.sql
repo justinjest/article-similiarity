f5801713f093132667089412d6858674292de624
justinjest <jest.cayaban@gmail.com>
2024-11-13 20:40:51 +0900

    Cleaned up working directory

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -3,6 +3,12 @@
 CREATE TABLE users (
 id uuid Primary Key
 );
+CREATE TABLE wordsInSet(
+    id uuid PRIMARY KEY,
+    word TEXT NOT NULL,
+    inDocs INTEGER NOT NULL
+);
 
 -- +goose Down
+DROP TABLE wordsInSet;
 DROP TABLE users;
